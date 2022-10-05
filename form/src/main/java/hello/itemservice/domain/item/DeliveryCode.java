8b8e7d23b9fb04dbcce35ab0bb8acb3184fd37de
KIMJINSU <rlawlstn3206@naver.com>
2022-10-05 21:56:26 +0900

    spring_study

---
+++ form/src/main/java/hello/itemservice/domain/item/DeliveryCode.java
@@ -0,0 +1,20 @@
+package hello.itemservice.domain.item;
+
+import lombok.Data;
+
+/**
+ * FAST:빠른배송
+ * NORMAL:일반배송
+ * SLOW:느린배송
+ */
+
+@Data
+public class DeliveryCode {
+    private String code;
+    private String displayName;
+
+    public DeliveryCode(String code, String displayName) {
+        this.code = code;
+        this.displayName = displayName;
+    }
+}
