2f195c2a9afb70a83e6027d4cbc7cd945201297b
dgjinsu <rlawlstn3206@naver.com>
2023-03-07 19:02:08 +0900

    spring_study

---
+++ typeconverter/src/main/java/hello/typeconverter/controller/HelloController.java
@@ -0,0 +1,26 @@
+package hello.typeconverter.controller;
+
+import hello.typeconverter.type.IpPort;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello-v1")
+    public String helloV1(HttpServletRequest request) {
+        String data = request.getParameter("data"); //문자 타입 조회
+        Integer intValue = Integer.valueOf(data); //숫자 타입으로 변경
+        System.out.println("intValue = " + intValue);
+        return "ok";
+    }
+
+    @GetMapping("/ip-port")
+    public String ipPort(@RequestParam IpPort ipPort) {
+        System.out.println("ipPort IP = " + ipPort.getIp());
+        System.out.println("ipPort PORT = " + ipPort.getPort());
+        return "ok";
+    }
+}
